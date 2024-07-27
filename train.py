04ed27029613e3ce5b032b38a2bd8d92075aebbc
Veera <veerakarthick609@gmail.com>
2024-07-27 22:08:07 +0530

    fixed weights init

--- train.py
+++ train.py
@@ -9,11 +9,12 @@ import tiktoken
 
 # -----------------------------------------
 
+
 class DataLoaderLite:
     def __init__(self, B, T):
         self.B = B
         self.T = T
-        
+
         with open("input.txt", "r") as file:
             text = file.read()
         enc = tiktoken.get_encoding("gpt2")
@@ -24,19 +25,20 @@ class DataLoaderLite:
 
         # state
         self.current_position = 0
-    
+
     def next_batch(self):
         B, T = self.B, self.T
-        buf = self.tokens[self.current_position : self.current_position + B*T+1]
+        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
         x = (buf[:-1]).view(B, T)
-        y = (buf[1: ]).view(B, T)
+        y = (buf[1:]).view(B, T)
 
-        self.current_position += B*T
+        self.current_position += B * T
 
-        if self.current_position + (B*T+1) > len(self.tokens):
+        if self.current_position + (B * T + 1) > len(self.tokens):
             self.current_position = 0
 
         return x, y
+
 
 # -----------------------------------------
 class CausalSelfAttention(nn.Module):
@@ -86,6 +88,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -137,6 +140,20 @@ class GPT(nn.Module):
 
         # weight sharing
         self.transformer.wte.weight = self.lm_head.weight
+
+        # init parameters
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, "NANOGPT_SCALE_INIT"):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
@@ -222,12 +239,13 @@ class GPT(nn.Module):
 
         return model
 
+
 # ---------------------------------------------------------------------------------------------------
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
-    device ="mps"
+    device = "mps"
 print(f"Using device :{device}")
 
 # ----------------------------------------------------------------------------------------------------
@@ -247,7 +265,9 @@ for i in range(50):
     optimizer.step()
     print(f"Epoch {i}/50. Loss: {loss.item()}")
 
-import sys; sys.exit(0)
+import sys
+
+sys.exit(0)
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -273,7 +293,7 @@ torch.cuda.manual_seed(42)
 
 while x.size(1) < max_length:
     with torch.no_grad():
-        logits,_ = model(x)  # (B, T, vocab_size)
+        logits, _ = model(x)  # (B, T, vocab_size)
 
         # consider the logit at the last position
         logits = logits[:, -1, :]  # (B, vocab_size)
@@ -287,10 +307,10 @@ while x.size(1) < max_length:
         topk_probs, topk_indices = torch.topk(probs, k=50, dim=-1)
 
         # select a token from the top-k probabilities
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        ix = torch.multinomial(topk_probs, 1)  # (B, 1)
 
         # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        xcol = torch.gather(topk_indices, -1, ix)  # (B, 1)
 
         # append to the sequnce
         x = torch.cat((x, xcol), dim=1)
