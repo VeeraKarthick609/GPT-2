0031e7f06c949acf936cc7711f285f8ecdfaa18c
Veera <veerakarthick609@gmail.com>
2024-07-27 15:12:12 +0530

    added weight sharing for lm_head and output embedding layers

--- train.py
+++ train.py
@@ -135,6 +135,9 @@ class GPT(nn.Module):
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        # weight sharing
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
         B, T = idx.size()
