885e09ab6a2e9b7de74d85968d87feacc887091b
Veera <veerakarthick609@gmail.com>
2024-07-27 14:58:24 +0530

    added custom data loader

--- train.py
+++ train.py
@@ -5,10 +5,40 @@ import torch.backends.mps
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import GPT2LMHeadModel
+import tiktoken
 
 # -----------------------------------------
 
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        
+        with open("input.txt", "r") as file:
+            text = file.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B*T)}")
 
+        # state
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T+1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1: ]).view(B, T)
+
+        self.current_position += B*T
+
+        if self.current_position + (B*T+1) > len(self.tokens):
+            self.current_position = 0
+
+        return x, y
+
+# -----------------------------------------
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
@@ -199,22 +229,15 @@ print(f"Using device :{device}")
 
 # ----------------------------------------------------------------------------------------------------
 
-import tiktoken
-enc = tiktoken.get_encoding("gpt2")
-with open("input.txt", "r") as file:
-    text = file.read()
+train_loader = DataLoaderLite(4, 32)
 
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:(B*T) + 1])
-x = buf[:-1].view(B, T).to(device)
-y= buf[1:].view(B, T).to(device)
 
 model = GPT(GPTConfig()).to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
